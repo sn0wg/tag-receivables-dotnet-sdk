a477a15961ae63e831fa9c40d3ecd98392e3a843
Guilherme Neves <guineves1997@gmail.com>
2021-02-09 14:52:10 -0300

    Tornada a classe de constantes estática

--- TagSDK/Constants/Constants.cs
+++ TagSDK/Constants/Constants.cs
@@ -1,13 +1,13 @@
 ﻿namespace TagSDK.Constants
 {
-    internal class Constants
+    internal static class Constants
     {
-        public class Authentication
+        public static class Authentication
         {
             public const string BasePath = "token";
         }
 
-        public class Consent
+        public static class Consent
         {
             public const string BasePath = "receivable/consent";
 
@@ -19,9 +19,10 @@
 
             public const string BasePathOptInParams = "receivable/consent/optin/parameters";
         }
-        public class Endorsement
+
+        public static class Endorsement
         {
-            public class Contract
+            public static class Contract
             {
                 public const string BasePath = "receivable/contract";
                 public const string BasePathHistory = "receivable/contract/history";
@@ -30,31 +31,30 @@
                 public const string BasePathProcessKey = "receivable/contract/processkey";
             }
 
-            public class Advancement
+            public static class Advancement
             {
                 public const string BasePath = "receivable/advancement";
             }
-
         }
 
-        public class CommercialEstablishment
+        public static class CommercialEstablishment
         {
             public const string BasePath = "customer/commercialEstablishment";
         }
 
-        public class Notification
+        public static class Notification
         {
             public const string BasePath = "receivable/notification";
             public const string BasePathProcessKey = "receivable/notification/processKey";
             public const string BasePathKey = "receivable/notification/key";
         }
 
-        public class Receivable
+        public static class Receivable
         {
             public const string BasePath = "receivable";
         }
 
-        public class Settlement
+        public static class Settlement
         {
             public const string BasePath = "receivable/settlement";
             public const string BasePathReject = "receivable/settlement/reject";
@@ -63,21 +63,21 @@
             public const string BasePathReference = "receivable/settlement/reference";
         }
 
-        public class Transaction
+        public static class Transaction
         {
             public const string BasePathTransaction = "receivable/transaction";
 
             public const string BasePathTransactionKey = "receivable/transaction/";
         }
 
-        public class Reconciliation
+        public static class Reconciliation
         {
             public const string BasePath = "receivable/reconciliation";
 
             public const string BasePathKey = "receivable/reconciliation/key";
         }
 
-        public class Position
+        public static class Position
         {
             public const string BasePath = "receivable/position";
             public const string BasePathKey = "receivable/position/key";
